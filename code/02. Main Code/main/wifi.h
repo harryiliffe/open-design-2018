2f4efe052074d1dcf9b8fdc5be8bb9cd53a77734
Harry Iliffe <iliffe.harry@gmail.com>
2018-02-13 01:29:30 +1300

    added ota and json parsing

--- code/02. Main Code/main/wifi.h
+++ code/02. Main Code/main/wifi.h
@@ -92,7 +92,7 @@ void startWiFi() { // Start a Wi-Fi access point, and try to connect to some giv
   Serial.print(ssid);
   Serial.println("\" started\r\n");
   //  wifiMulti.addAP("Ventilation Nation", "Somebodyoncetoldmetheworldisgonnar0llme");   // add Wi-Fi networks you want to connect to
-      wifiMulti.addAP("Flat-Queens", "hellotessa");
+  wifiMulti.addAP("Flat-Queens", "hellotessa");
   //  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
 
 
@@ -113,6 +113,32 @@ void startWiFi() { // Start a Wi-Fi access point, and try to connect to some giv
     Serial.print(WiFi.localIP());
   }
   Serial.println("\r\n");
+}
+
+
+void startOTA() { // Start the OTA service
+  ArduinoOTA.setHostname(OTAName);
+  ArduinoOTA.setPassword(OTAPassword);
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\r\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready\r\n");
 }
 
 void startWebSocket() { // Start a WebSocket server
@@ -198,11 +224,14 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
       }
       break;
     case WStype_TEXT :                  // if new text data is received
-
-
-
+      JsonObject& root = jsonBuffer.parseObject(payload);
+      const char* type = root["type"];
+      if (type == "modechange") {
+        modeEnabled = root["mode"];
+        leds_modeChange(modes[modeEnabled].color);
+        modeInit();
+      }
       break;
-
   }
 }
 
