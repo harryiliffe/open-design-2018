13ae038c23e9ed482ff237827e3a8dec5b3c83f3
Harry Iliffe <iliffe.harry@gmail.com>
2018-02-13 19:58:18 +1300

    added sound + game "GregSays"

---
+++ code/02. Main Code/main/gregsays.h
@@ -0,0 +1,85 @@
+
+
+#define PATTERNLENGTH 10
+
+int pattern[PATTERNLENGTH];
+
+bool playingSequence;
+bool respondSequence;
+
+int gameSpeed = 1000;
+int level;
+int noteDuration;
+int patternProgress;
+int respondProgress;
+
+Ticker patternTimer;
+
+void gregSays_setup() {
+  randomSeed(analogRead(A0));
+  
+  for (int i = 0; i < PATTERNLENGTH; i++) {
+    pattern[i] = int(random(NUM_LEDS));
+    Serial.printf("Got random number %i\n", pattern[i]);
+  }
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  for(int i=0; i<NUM_LEDS;i++){
+    sound_playTone(i, 470, false, true);
+    delay(500);
+  }
+  level = 1;
+  playingSequence = true;
+  patternProgress = 0;
+  gameSpeed = 800;
+  noteDuration = gameSpeed*0.875;
+  delay(800);
+  fill_solid(leds, NUM_LEDS, CRGB::Green);
+  delay(800);
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  delay(800);
+  patternTimer.attach_ms(gameSpeed, greySays_playLevel);
+  greySays_playLevel();
+}
+
+void greySays_playLevel(){
+  Serial.printf("Playing note %i from sequence: %i\n", pattern[patternProgress],patternProgress);
+  if(patternProgress < level){
+    sound_playTone(pattern[patternProgress], noteDuration, false, true);
+    patternProgress++;
+  }
+  if(patternProgress == level){
+    playingSequence=false;
+    patternTimer.detach();
+    greySays_respond();
+  }
+     
+}
+
+
+void greySays_respond(){
+  if(!respondSequence){
+    Serial.println("Waiting for Responses");
+    respondSequence = true;
+    respondProgress = 0;
+  }
+  if(buttonState[pattern[respondProgress]].touch && !buttonState[pattern[respondProgress]].touchRegistered && touch_exclusive(pattern[respondProgress], 0, 5) && !playingNote ){
+    buttonState[pattern[respondProgress]].touchRegistered = true;
+    sound_playTone(pattern[respondProgress], noteDuration, false, true);
+    Serial.printf("Responding note %i from sequence: %i\n", pattern[respondProgress],respondProgress);
+    respondProgress++;
+  }
+  if(respondProgress == level && !playingNote){
+    respondSequence = false;
+    level++;
+    Serial.printf("Congrats Level Up: Level %i\n", level);
+    delay(300);
+    playingSequence = true;
+    patternProgress = 0;
+    greySays_playLevel();
+    patternTimer.attach_ms(gameSpeed, greySays_playLevel);
+  }
+}
+
+
+
+
--- code/02. Main Code/main/sound.h
+++ code/02. Main Code/main/sound.h
@@ -1,1 +1,37 @@
+#define SPEAKER 16
+
+bool playingNote = false;
+Ticker noteTimer;
+Ticker lightTimer;
+
+int tones[] = {261, 294, 330, 349, 392};
+CRGB toneColors[] = {CRGB::Red, CRGB::Green, CRGB::Yellow, CRGB::Blue, CRGB::Purple};
+//            mid C  D    E    F    G
+
+
+void sound_playTone(int toneRef, int duration, bool overide, bool lights) {
+  if (!playingNote || overide) {
+    Serial.printf("Playing Note %i\n", toneRef);
+    playingNote = true;
+    noTone(SPEAKER);
+    tone(SPEAKER, tones[toneRef]);
+    if (lights) {
+      leds[toneRef] = toneColors[toneRef];
+      FastLED.show();
+      lightTimer.once_ms(duration - 30, sound_stopLights, toneRef);
+    }
+    noteTimer.once_ms(duration, sound_stopTone, SPEAKER);
+  }
+}
+
+void sound_stopLights(int light) {
+  leds[light] = CRGB::Black;
+  FastLED.show();
+}
+
+void sound_stopTone(int pin){
+  noTone(pin);
+  Serial.println("Stopping Note");
+  playingNote = false;
+}
 
